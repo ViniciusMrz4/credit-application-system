ad8f8f43b9b5dbcd1089ab94e6ac6aca8f45eaa0
ViniciusMrz4 <viniciusmarinrod@gmail.com>
2024-01-07 19:39:28 -0300

    Interfaces

---
+++ credit-application-system/src/main/kotlin/me/project/creditapplicationsystem/repository/CreditRepository.kt
@@ -0,0 +1,9 @@
+package me.project.creditapplicationsystem
+
+import me.project.creditapplicationsystem.entity.Credit
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CreditRepository: JpaRepository<Credit, Long> {
+}
